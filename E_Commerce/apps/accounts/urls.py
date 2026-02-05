f0f577fe5b05c5b38ee4811be2838c172a5678f7
simon geletaw <simongeletaw2@gmail.com>
2026-02-06 02:07:57 +0300

    feat: finished swagger

--- E_Commerce/apps/accounts/urls.py
+++ E_Commerce/apps/accounts/urls.py
@@ -3,7 +3,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import ChangePasswordView, MeView, RegisterView, UserListView
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('me/', MeView.as_view(), name='me'),
+    path('Me/', MeView.as_view(), name='me'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('change-password/', ChangePasswordView.as_view(), name='change_password'),
--- E_Commerce/apps/products/urls.py
+++ E_Commerce/apps/products/urls.py
@@ -3,6 +3,7 @@ from .views import (
     ProductDetailView,
     ProductListView,
     ProductUpdateView,
+    ProductDeleteView
 )
 from django.urls import path
 urlpatterns = [
@@ -10,4 +11,5 @@ urlpatterns = [
     path('create/', ProductCreateView.as_view(), name='product_create'),
     path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+    path('<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
 ]
